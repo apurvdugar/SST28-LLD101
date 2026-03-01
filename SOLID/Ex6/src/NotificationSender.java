b78f8822ce42ca7045125a912c5a54e16f842536
Apurv Dugar <dugarapurv27@gmail.com>
2026-03-01 22:44:47 +0530

    Ex6 done

--- SOLID/Ex6/src/NotificationSender.java
+++ SOLID/Ex6/src/NotificationSender.java
@@ -1,5 +1,19 @@
 public abstract class NotificationSender {
     protected final AuditLog audit;
-    protected NotificationSender(AuditLog audit) { this.audit = audit; }
-    public abstract void send(Notification n);
+    protected NotificationSender(AuditLog audit) { 
+        this.audit = audit; 
+    }
+    
+    // made this final to prevent overriding and breaking the audit log
+    public final void send(Notification n) {
+         if (n == null) {
+            System.out.println("ERROR: invalid notification");
+            audit.add("invalid");
+            return;
+        }
+
+        doSend(n);
+    }
+
+    public abstract void doSend(Notification n);
 }
--- SOLID/Ex6/src/SmsSender.java
+++ SOLID/Ex6/src/SmsSender.java
@@ -1,9 +1,16 @@
 public class SmsSender extends NotificationSender {
-    public SmsSender(AuditLog audit) { super(audit); }
+    public SmsSender(AuditLog audit) { 
+        super(audit); 
+    }
 
     @Override
-    public void send(Notification n) {
+    public void doSend(Notification n) {
         // Ignores subject; base type doesn't clarify expectations (smell)
+        if (n.phone == null || n.body == null) {
+            System.out.println("SMS ERROR: invalid notification");
+            audit.add("sms error");
+            return;
+        }
         System.out.println("SMS -> to=" + n.phone + " body=" + n.body);
         audit.add("sms sent");
     }
